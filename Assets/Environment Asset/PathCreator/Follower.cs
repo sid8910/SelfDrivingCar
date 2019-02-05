3e60fb10a140599f5a964f38604fe3bf00ea8372
DESKTOP-8PFDKFF\DELL <shubham.baghe8910@gmail.com>
2019-02-06 03:11:38 +0700

    adding follower script

---
+++ Assets/Environment Asset/PathCreator/Follower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Follower : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    Vector3 distanceTravelled;
+    float distanceTravelledact;
+    public float speed = 5;
+    public float[] waypoint = new float[1000];
+    int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*      distanceTravelled += speed * Time.deltaTime;
+                transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+                print(distanceTravelled);
+
+        */
+        distanceTravelledact = distanceTravelledact + (float)3;
+        distanceTravelled = pathCreator.path.GetPointAtDistance(distanceTravelledact);
+        //waypoint[i] = distanceTravelled;
+        print(distanceTravelled);
+        i++;
+    }
+}
