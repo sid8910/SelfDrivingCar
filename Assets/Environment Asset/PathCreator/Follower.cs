9a36a2d9aa4030721e9b6f7b4b7a74a5446f295e
DESKTOP-8PFDKFF\DELL <shubham.baghe8910@gmail.com>
2019-02-06 20:56:25 +0530

    waypoints

--- Assets/Environment Asset/PathCreator/Follower.cs
+++ Assets/Environment Asset/PathCreator/Follower.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 using PathCreation;
 
 public class Follower : MonoBehaviour
-{
+{   
     public PathCreator pathCreator;
-    Vector3 distanceTravelled;
-    float distanceTravelledact;
+    float distanceTravelled = 0;
     public float speed = 5;
-    public float[] waypoint = new float[1000];
+    Vector3 waypoint, prevWaypoint;
+    //public float[] waypoint = new float[1000];
     int i = 0;
+    float distanceTravelled1;
     // Start is called before the first frame update
     void Start()
     {
+        Vector3 waypoint;
+        while (true)
+        {
+            waypoint = pathCreator.path.GetPointAtDistance(distanceTravelled, EndOfPathInstruction.Stop);
+            distanceTravelled += 5;
+            print(waypoint);
+            if (waypoint == prevWaypoint)
+            {
+                break;
+            }
+            prevWaypoint = waypoint;
+        }
 
        
     }
@@ -23,15 +36,13 @@ public class Follower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*      distanceTravelled += speed * Time.deltaTime;
-                transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
-                print(distanceTravelled);
-
-        */
+        /*distanceTravelled1 += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled1);
         distanceTravelledact = distanceTravelledact + (float)3;
         distanceTravelled = pathCreator.path.GetPointAtDistance(distanceTravelledact);
         //waypoint[i] = distanceTravelled;
         print(distanceTravelled);
-        i++;
+        i++;        */
+
     }
 }
