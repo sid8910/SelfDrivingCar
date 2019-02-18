feb68cd5d6e5b6dc0e0f4f7b99a4db83c8a3c947
DESKTOP-8PFDKFF\DELL <shubham.baghe8910@gmail.com>
2019-02-19 00:36:06 +0530

    environment

---
+++ Assets/Environment Asset/Signals/Models and Textures/Signal1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signal1 : MonoBehaviour
+{
+    public Light spotlightG1;
+    public Light spotlightG2;
+    public Light spotlightR1;
+    public Light spotlightR2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spotlightG1.intensity = 10f;
+        spotlightG2.intensity = 10f;
+        spotlightR1.intensity = 0f;
+        spotlightR2.intensity = 0f;
+        StartCoroutine(InitiateSignal());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator InitiateSignal()
+    {
+        while (spotlightG1.intensity == 10f && spotlightG2.intensity == 10f)
+        {
+            yield return new WaitForSeconds(5);
+            spotlightG1.intensity = 0f;
+            spotlightG2.intensity = 0f;
+            spotlightR1.intensity = 10f;
+            spotlightR2.intensity = 10f;
+            yield return new WaitForSeconds(5);
+            spotlightG1.intensity = 10f;
+            spotlightG2.intensity = 10f;
+            spotlightR1.intensity = 0f;
+            spotlightR2.intensity = 0f;
+        }
+    }
+}
